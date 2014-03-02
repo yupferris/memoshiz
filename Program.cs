d247e8363324652a971e173279a2d944062e3b11
ferris <jake@outracks.com>
2014-03-02 17:12:00 +0100

    started repo wheeee

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoShiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR: " + e);
+            }
+        }
+    }
+}
