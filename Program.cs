5100207a13bf19a8181623883586d44b87c9f99f
ferris <jake@outracks.com>
2014-03-02 17:23:26 +0100

    lol

--- Program.cs
+++ Program.cs
@@ -12,6 +12,35 @@ namespace MemoShiz
         {
             try
             {
+                int numSequences = int.Parse(args[0]);
+                int sequenceLength = int.Parse(args[1]);
+
+                var sequences = new char[numSequences, sequenceLength];
+
+                var r = new Random();
+                for (int i = 0; i < numSequences; i++)
+                {
+                    for (int j = 0; j < sequenceLength; j++)
+                    {
+                        var c = (char)('A' + r.Next(26));
+                        sequences[i, j] = c;
+                        Console.Write(c);
+                        if ((j & 1) != 0)
+                            Console.Write(' ');
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+
+                Console.ReadKey();
+
+                Console.Clear();
+                for (int i = 0; i < numSequences; i++)
+                {
+                    for (int j = 0; j < sequenceLength; j++)
+                    {
+                    }
+                }
             }
             catch (Exception e)
             {
